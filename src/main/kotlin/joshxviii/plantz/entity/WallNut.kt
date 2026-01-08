f97e669dea91b7959c0f0a80c5e24e21bf90bcf6
Josh <125091065+joshxviii@users.noreply.github.com>
2026-01-08 13:29:04 -0500

    Wallnut immune to arrows

--- src/main/kotlin/joshxviii/plantz/entity/WallNut.kt
+++ src/main/kotlin/joshxviii/plantz/entity/WallNut.kt
@@ -1,10 +1,12 @@
 package joshxviii.plantz.entity
 
 import joshxviii.plantz.PazEntities
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.damagesource.DamageSource
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
-import net.minecraft.world.entity.ai.control.LookControl
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal
+import net.minecraft.world.entity.projectile.arrow.AbstractArrow
 import net.minecraft.world.level.Level
 import java.util.*
 
@@ -18,4 +20,9 @@ class WallNut(type: EntityType<out Plant>, level: Level) : Plant(PazEntities.WAL
 
     // solid collision
     override fun canBeCollidedWith(other: Entity?): Boolean = this.isAlive
+
+    override fun hurtServer(level: ServerLevel, source: DamageSource, damage: Float): Boolean {
+        return if (source.directEntity is AbstractArrow) false
+        else super.hurtServer(level, source, damage)
+    }
 }
